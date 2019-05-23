6265f0c32e6cd8c2ff905e83a969d3b573f2e3c5
Nikhil <nikhilthakur050@gmail.com>
2019-05-23 07:41:10 +0530

    [Nikhil] hello

---
+++ src/com/lambton/CollegeData.java
@@ -0,0 +1,9 @@
+package com.lambton;
+
+public class CollegeData {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("abcd");
+    }
+}
+
